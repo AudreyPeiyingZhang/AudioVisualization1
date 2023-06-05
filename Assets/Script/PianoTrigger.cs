c34df67a66773a84ed31b2c93c40198eb1ca6bd2
AudreyPeiyingZhang <DTNVPZ1@UCL.AC.UK>
2023-06-05 16:50:21 +0100

    update

--- Assets/Script/PianoTrigger.cs
+++ Assets/Script/PianoTrigger.cs
@@ -30,8 +30,18 @@ public class PianoTrigger : MonoBehaviour
         GetHkeyDown();
         GetJkeyDown();
         GetKkeyDown();
+        GetQkeyDown();
+        GetWkeyDown();
+        GetEkeyDown();
+        GetRkeyDown();
+        GetTkeyDown();
+        GetYkeyDown();
+        GetUkeyDown();
+        GetIkeyDown();
+        GetOkeyDown();
 
-       
+
+
     }
     public void GetAkeyDown()
     {
@@ -44,7 +54,7 @@ public class PianoTrigger : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.A))
         {
             _keyButtons[0].GetComponent<Image>().color = Color.white;
-            _audiosource[0].Stop();
+            
         }
         
     }
@@ -59,7 +69,7 @@ public class PianoTrigger : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.S))
         {
             _keyButtons[1].GetComponent<Image>().color = Color.white;
-            _audiosource[1].Stop();
+            
         }
 
     }
@@ -75,7 +85,7 @@ public class PianoTrigger : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.D))
         {
             _keyButtons[2].GetComponent<Image>().color = Color.white;
-            _audiosource[2].Stop();
+            
         }
 
     }
@@ -91,7 +101,7 @@ public class PianoTrigger : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.F))
         {
             _keyButtons[3].GetComponent<Image>().color = Color.white;
-            _audiosource[3].Stop();
+            
         }
 
     }
@@ -107,7 +117,7 @@ public class PianoTrigger : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.G))
         {
             _keyButtons[4].GetComponent<Image>().color = Color.white;
-            _audiosource[4].Stop();
+           
         }
 
     }
@@ -123,7 +133,7 @@ public class PianoTrigger : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.H))
         {
             _keyButtons[5].GetComponent<Image>().color = Color.white;
-            _audiosource[5].Stop();
+            
         }
 
     }
@@ -139,7 +149,6 @@ public class PianoTrigger : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.J))
         {
             _keyButtons[6].GetComponent<Image>().color = Color.white;
-            _audiosource[6].Stop();
         }
 
     }
@@ -155,7 +164,143 @@ public class PianoTrigger : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.K))
         {
             _keyButtons[7].GetComponent<Image>().color = Color.white;
-            _audiosource[7].Stop();
+        }
+
+    }
+
+
+    public void GetQkeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _keyButtons[8].GetComponent<Image>().color = _color;
+            _audiosource[8].Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            _keyButtons[8].GetComponent<Image>().color = Color.white;
+        }
+
+    }
+
+    public void GetWkeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _keyButtons[9].GetComponent<Image>().color = _color;
+            _audiosource[9].Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            _keyButtons[9].GetComponent<Image>().color = Color.white;
+        }
+
+    }
+
+    public void GetEkeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _keyButtons[10].GetComponent<Image>().color = _color;
+            _audiosource[10].Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            _keyButtons[10].GetComponent<Image>().color = Color.white;
+        }
+
+    }
+
+    public void GetRkeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _keyButtons[11].GetComponent<Image>().color = _color;
+            _audiosource[11].Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            _keyButtons[11].GetComponent<Image>().color = Color.white;
+        }
+
+    }
+
+    public void GetTkeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _keyButtons[12].GetComponent<Image>().color = _color;
+            _audiosource[12].Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            _keyButtons[12].GetComponent<Image>().color = Color.white;
+        }
+
+    }
+
+    public void GetYkeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            _keyButtons[13].GetComponent<Image>().color = _color;
+            _audiosource[13].Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            _keyButtons[13].GetComponent<Image>().color = Color.white;
+        }
+
+    }
+
+    public void GetUkeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            _keyButtons[14].GetComponent<Image>().color = _color;
+            _audiosource[14].Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            _keyButtons[14].GetComponent<Image>().color = Color.white;
+        }
+
+    }
+
+    public void GetIkeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            _keyButtons[15].GetComponent<Image>().color = _color;
+            _audiosource[15].Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            _keyButtons[15].GetComponent<Image>().color = Color.white;
+        }   
+
+    }
+
+
+    public void GetOkeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            _keyButtons[16].GetComponent<Image>().color = _color;
+            _audiosource[16].Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.O))
+        {
+            _keyButtons[16].GetComponent<Image>().color = Color.white;
         }
 
     }
